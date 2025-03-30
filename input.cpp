5874e7ce963e1f31715d0ad901ceb644e0d52e81
Aniket <aniket27.saxena@gmail.com>
2025-03-30 09:21:17 +0530

    added  10x10 matrices example to test

--- input.cpp
+++
@@ -1,9 +0,0 @@
-// Sample input file for the PIM compiler
-
-// Correct format with spaces
-std::vector<std::vector<int>> matrix_a = {{ 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 }};
-std::vector<std::vector<int>> matrix_b = {{ 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 }};
-// Other code can exist here, but it will be ignored by the parser.
-int some_other_function() {
-    return 0;
-} 
